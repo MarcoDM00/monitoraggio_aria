f0a9b80590d8a84741b749bd319784a7f5e1f079
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-04-14 20:45:33 +0000

    quasi finito

---
+++ src/app/Record.ts
@@ -0,0 +1,6 @@
+export interface Record {
+    data: string;
+    descrizione: string;
+    previsione: string;
+    tendenza: string;
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { Record } from './Record';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  sub:Subscription;
+  url:string = "https://dati.comune.milano.it/datastore/dump/eda4e6bd-c037-4453-9e4d-4429e41659c1?format=json";
+  records:Record[] = [];
+  indice:number = 0;
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.records.push({data:"", descrizione:"", previsione:"", tendenza:""});
+    this.sub = this.http.get<any>(this.url).subscribe(
+      res => {
+        this.records = [];
+        [...res['records']].forEach(r => {
+          let data = new Date(r[1]);
+          let str = data.getDate() + "/";
+          str += (data.getMonth() + 1 < 10) ? "0" + (data.getMonth() + 1) : data.getMonth() + 1;
+          str += "/" + data.getFullYear();
+          this.records.push({data: str, descrizione: r[2], previsione: r[3], tendenza: r[4]});
+        });;
+      }
+    );
+  }
+
+  change(type:number) {
+    if (type == 0) {
+      this.indice -= 1;
+      if (this.indice == -1) this.indice = this.records.length - 1;
+    } else {
+      this.indice += 1;
+      if (this.indice == this.records.length) this.indice = 0;
+    }
+  }
+}
